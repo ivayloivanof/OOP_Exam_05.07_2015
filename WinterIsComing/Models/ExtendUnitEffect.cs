dc9960875660ed39f913a49494505426d076a79f
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-05 22:51:06 +0300

    add ExtendUnitEffect

---
+++ WinterIsComing/Models/ExtendUnitEffect.cs
@@ -0,0 +1,17 @@
+﻿namespace WinterIsComing.Models
+{
+    using System.Collections.Generic;
+
+    using Contracts;
+
+    class ExtendUnitEffect : IUnitEffector
+    {
+        public void ApplyEffect(IEnumerable<IUnit> units)
+        {
+            foreach (IUnit unit in units)
+            {
+                unit.HealthPoints += 50;
+            }
+        }
+    }
+}
