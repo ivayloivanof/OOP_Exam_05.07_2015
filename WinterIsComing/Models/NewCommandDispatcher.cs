2b8910d4d86eff69ae59e96bdfdc41b61ddf54e2
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-07-05 22:43:03 +0300

    Using base Dispatch Command method

--- WinterIsComing/Models/NewCommandDispatcher.cs
+++ WinterIsComing/Models/NewCommandDispatcher.cs
@@ -1,25 +1,13 @@
 ﻿namespace WinterIsComing.Models
 {
-    using System;
     using Core;
 
     class NewCommandDispatcher : CommandDispatcher
     {
-        public NewCommandDispatcher() : base()
-        {
-        }
 
         public override void DispatchCommand(string[] commandArgs)
         {
-            string commandName = commandArgs[0];
-            if (!this.commandsByName.ContainsKey(commandName))
-            {
-                throw new NotSupportedException(
-                    "Command is not supported by engine");
-            }
-
-            var command = this.commandsByName[commandName];
-            command.Execute(commandArgs);
+            base.DispatchCommand(commandArgs);
         }
 
         public override void SeedCommands()
